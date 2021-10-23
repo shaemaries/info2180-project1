d478a0c63b43239a9f8a4fc58320f2ae81a6f7af
Shae-Marie Small <shaemarie.small@gmail.com>
2021-10-23 19:20:50 -0400

    Added initial starter files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,47 @@
 /* Add your JavaScript to this file */
+"use strict"
+
+window.onload = function(){
+    var form = document.getElementsByTagName('form')[0]
+	document.getElementsByTagName('button')[0].addEventListener("click", validateForm)
+}
+
+function validateForm(){
+    var validEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    var inputEmail= document.getElementById("email").value.trim()
+
+    event.preventDefault();
+    event.stopPropagation()
+   
+
+    if(validEmail.test(inputEmail) && inputEmail !=""){
+         setMessage('Thank you! Your email adress '+ inputEmail +' has been added to our mailing list', "valid")
+         document.getElementById("email").value = ""
+         return 
+    }
+
+
+   setMessage('Please enter a valid email address',"invalid")   
+
+
+}
+
+function setMessage(message, status){
+    var messageContainer = document.getElementsByClassName("message")[0]
+    messageContainer.innerHTML=message
+    messageContainer.style.backgroundColor="white"
+    messageContainer.style.marginTop ="10px"
+    messageContainer.style.padding="5px"
+
+    if (status == "invalid"){
+        messageContainer.style.color="red"
+    }
+    else{
+        messageContainer.style.color="green"
+    }
+
+    setTimeout(function(){
+        messageContainer.style.display='none';
+    },3000)
+
+}
